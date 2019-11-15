845f84f8fbafc3753ec6bc332956ca8a7bea7b43
Mouse <mouse0w0@qq.com>
2019-11-15 14:43:58 +0800

    Init

---
+++ src/main/java/com/github/mouse0w0/example/Example.java
@@ -0,0 +1,14 @@
+package com.github.mouse0w0.example;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Example.MOD_ID)
+public class Example {
+    public static final String MOD_ID = "example";
+
+    @Mod.EventHandler
+    public void onPreInit(FMLPreInitializationEvent event) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/github/mouse0w0/example/mixin/MixinMinecraft.java
@@ -0,0 +1,43 @@
+package com.github.mouse0w0.example.mixin;
+
+import net.minecraft.client.Minecraft;
+import org.apache.logging.log4j.Logger;
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.PixelFormat;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(Minecraft.class)
+public abstract class MixinMinecraft {
+
+    @Shadow
+    private static Logger LOGGER;
+
+    @Shadow
+    private boolean fullscreen;
+
+    @Overwrite
+    private void createDisplay() throws LWJGLException {
+        Display.setResizable(true);
+        Display.setTitle("MyCustomTitle");
+        try {
+            Display.create((new PixelFormat()).withDepthBits(24));
+        } catch (LWJGLException lwjglexception) {
+            LOGGER.error("Couldn't set pixel format", lwjglexception);
+            try {
+                Thread.sleep(1000L);
+            } catch (InterruptedException e) {
+            }
+            if (this.fullscreen) {
+                this.updateDisplayMode();
+            }
+            Display.create();
+        }
+    }
+
+    @Shadow
+    private void updateDisplayMode() {
+    }
+}
