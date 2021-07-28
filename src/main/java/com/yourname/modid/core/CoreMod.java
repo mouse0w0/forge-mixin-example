7d537ff5162e70b8fbe675b61d13828e1d65d35b
Snownee <mjy523@126.com>
2021-07-29 06:26:56 +0800

    Fix crash on dedicated server

--- src/main/java/com/yourname/modid/core/CoreMod.java
+++ src/main/java/com/yourname/modid/core/CoreMod.java
@@ -1,35 +1,16 @@
 package com.yourname.modid.core;
 
-import net.minecraftforge.fml.relauncher.CoreModManager;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
-import org.apache.logging.log4j.LogManager;
 import org.spongepowered.asm.launch.MixinBootstrap;
 
 import javax.annotation.Nullable;
-import java.io.File;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.security.CodeSource;
 import java.util.Map;
 
+@IFMLLoadingPlugin.Name("YourModName")
 public class CoreMod implements IFMLLoadingPlugin {
 
     public CoreMod() {
         MixinBootstrap.init();
-
-        CodeSource codeSource = this.getClass().getProtectionDomain().getCodeSource();
-        if (codeSource != null) {
-            URL location = codeSource.getLocation();
-            try {
-                File file = new File(location.toURI());
-                if (file.isFile()) {
-                    CoreModManager.getReparseableCoremods().remove(file.getName());
-                }
-            } catch (URISyntaxException ignored) {}
-        } else {
-            LogManager.getLogger().warn("No CodeSource, if this is not a development environment we might run into problems!");
-            LogManager.getLogger().warn(this.getClass().getProtectionDomain());
-        }
     }
 
     @Override
